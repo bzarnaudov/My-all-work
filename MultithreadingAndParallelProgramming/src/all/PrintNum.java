b5df8df9e264a809e1d448abe5a501e40375aa56
creature5 <creature_bg1985@yahoo.com>
2015-11-29 00:12:52 +0200

    update

--- MultithreadingAndParallelProgramming/src/all/PrintNum.java
+++ MultithreadingAndParallelProgramming/src/all/PrintNum.java
@@ -2,7 +2,7 @@ package all;
 
 public class PrintNum implements Runnable {
 
-	private  int lastNum;
+	private int lastNum;
 	
 	public PrintNum(int n) {
 		lastNum = n;
@@ -10,8 +10,15 @@ public class PrintNum implements Runnable {
 	
 	@Override
 	public void run() {
-		for (int i = 1; i <= lastNum; i++) {
-			System.out.print(" " + i);
-		}
+//		Thread thread4 = new Thread(new PrintChar('c', 40));
+//		thread4.start();
+//		try {
+			for (int i = 1; i <= lastNum; i++) {
+				System.out.print(" " + i);
+//				if (i == 50) thread4.join();
+			}
+//		} catch (InterruptedException ex) {
+			
+//		}
 	}
 }
--- MultithreadingAndParallelProgramming/src/all/TaskThreadDemo.java
+++ MultithreadingAndParallelProgramming/src/all/TaskThreadDemo.java
@@ -4,16 +4,22 @@ public class TaskThreadDemo {
 
 	public static void main(String[] args) {
 		// Create tasks
-		Runnable printA = new PrintChar('a', 100);
-		Runnable printB = new PrintChar('b', 100);
-		Runnable print100 = new PrintNum(100);
+		Runnable printA = new PrintChar('a', 1000);
+		Runnable printB = new PrintChar('b', 1000);
+		Runnable print100 = new PrintNum(1000);
 		
 		Thread thread1 = new Thread(printA);
 		Thread thread2 = new Thread(printB);
 		Thread thread3 = new Thread(print100);
 		
+
+		thread1.setPriority(Thread.MIN_PRIORITY);
+		System.out.println(thread1.getPriority());
 		thread1.start();
+		thread2.setPriority(Thread.MAX_PRIORITY);
+		System.out.println(thread2.getPriority());
 		thread2.start();
+		thread3.setPriority(Thread.MAX_PRIORITY);
 		thread3.start();
 	}
 }
